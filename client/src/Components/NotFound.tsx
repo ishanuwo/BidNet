646dea9a50f95b56ff278c5788bbe59c5593fd4b
Ahmedelj <ahmedeljannoun@gmail.com>
2025-04-02 18:50:49 -0400

    custom notfound page for the app

---
+++ client/src/Components/NotFound.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+  
+
+const NotFound: React.FC = () => (
+  <div className="not-found card">
+    <h2>404 - Page Not Found</h2>
+    <p>The page you are looking for does not exist.</p>
+    <Link to="/" className="button">Return to Home</Link>
+  </div>
+);
+
+export default NotFound;
